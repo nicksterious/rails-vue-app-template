12c5fb53d12e3fee2c8a973d9d7d128ea5eff595
nicksterious <nick@flinkwise.com>
2022-09-03 13:21:11 +0000

    ensure genres are unique

--- src/backend/app/models/genre.rb
+++ src/backend/app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ApplicationRecord
     validates_presence_of :name, allow_blank: false
-    validates_uniqueness_of :name
+    validates_uniqueness_of :name, case_sensitive: false
 
     has_and_belongs_to_many :movies
 
--- src/backend/spec/models/genre_spec.rb
+++ src/backend/spec/models/genre_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Genre, type: :model do
 
     describe "validations" do
 	it { should validate_presence_of :name }
-	it { should validate_uniqueness_of :name }
+	it { should validate_uniqueness_of(:name).case_insensitive }
     end
 
 end
