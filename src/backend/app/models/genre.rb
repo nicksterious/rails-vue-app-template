086c850c69bc41d464f924eb2ecbe6e3bfb60bdd
nicksterious <nick@flinkwise.com>
2022-09-03 13:15:58 +0000

    update genre model to validate name uniqueness

--- src/backend/app/models/genre.rb
+++ src/backend/app/models/genre.rb
@@ -1,5 +1,6 @@
 class Genre < ApplicationRecord
     validates_presence_of :name, allow_blank: false
+    validates_uniqueness_of :name
 
     has_and_belongs_to_many :movies
 
--- src/backend/spec/models/genre_spec.rb
+++ src/backend/spec/models/genre_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Genre, type: :model do
 
     describe "validations" do
 	it { should validate_presence_of :name }
+	it { should validate_uniqueness_of :name }
     end
 
 end
