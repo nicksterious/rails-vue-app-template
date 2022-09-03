1a1d5dbec987b390571d8e7815dcb8e14d0a73cf
nicksterious <nick@flinkwise.com>
2022-09-03 14:28:30 +0000

    import service for actors and movies

--- src/backend/app/services/movie_service.rb
+++ src/backend/app/services/movie_service.rb
@@ -8,14 +8,13 @@ class MovieService < ApiService
 	    .transform_keys { |key| key.to_s.underscore }
 	    .deep_symbolize_keys
 
-
 	builder = MovieBuilder.new( movie.except(:genres, :director, :actor_ids) )
-	
-	p movie
+
 	builder.create_additional_data(
 	    movie[:actor_ids].collect{ |i| { id: i } },
 	    movie[:director].split(",").collect{ |n| n.strip },
 	    movie[:genres]
 	)
+	# could use Kiba for this
     end
 end
