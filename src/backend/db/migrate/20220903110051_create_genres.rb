e215b1dad7343e4f597239b01a8dac259e13ce1b
nicksterious <nick@flinkwise.com>
2022-09-04 06:54:31 +0000

    added todo

--- src/backend/db/migrate/20220903110051_create_genres.rb
+++ src/backend/db/migrate/20220903110051_create_genres.rb
@@ -2,6 +2,7 @@ class CreateGenres < ActiveRecord::Migration[6.1]
   def change
     create_table :genres do |t|
       t.string :name
+      # TODO index on name for RoR-side query object searches
     end
   end
 end
